239cfb1855cf39b080ae8162cd91a7098b4f0f2b
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2018-07-29 19:47:57 +0700

    BE-2.4: Đăng tin job

---
+++ api/controllers/Job/create.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * TODO: 
+ * desciption: only get companyId from body now, wait for login case finish -> change companyId to req.user.id
+ * 
+ */
+
+module.exports = async function(req, res) {
+  try {
+    const { skillIds, title, description, status, categoryId, companyId } = req.allParams();
+
+    const job = await Job.create({
+      company: companyId,
+      title,
+      description,
+      status,
+      category: categoryId,
+      skills: skillIds
+    }).fetch();
+  
+    if (!job) {
+      return res.serverError({
+        err: 'could not create Job model'
+      })
+    }
+
+    res.ok(job);
+  } catch (err) {
+    res.serverError({err});
+  }
+}
