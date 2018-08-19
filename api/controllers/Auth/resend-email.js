54682d3ced4ae22f805a6da495b94b881167b1c8
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2018-08-19 12:31:28 +0700

    change message

--- api/controllers/Auth/resend-email.js
+++ api/controllers/Auth/resend-email.js
@@ -3,61 +3,42 @@ module.exports = async function (req, res) {
   const { email } = req.body;
   let userInfo;
 
-  if (role === 'company') {
-    if (!email) {
-      return res.badRequest({
-        message: "Missing parameters."
-      });
-    }
+  if (!email) {
+    return res.badRequest({
+      message: "Missing parameters."
+    });
+  }
 
+  if (role === 'company') {
     try {
       userInfo = await Company.findOne({ email });
-      if (!userInfo) {
-        return res.badRequest({
-          message: "This email is not exists."
-        });
-      } else {
-        const decodedInfo = _.assign({}, _.pick(userInfo, ['id', 'email']), { role: 'company' });
-        const verificationToken = JwtService.issue(decodedInfo, { expiresIn: '1h' });
-
-        EmailService.sendToUser(userInfo, 'verify-company-email', {
-          // verificationLink: `${process.env.WEB_URL}/email/verify?token=${verificationToken}`,
-          verificationLink: `${process.env.API_URL}/auth/verify?token=${verificationToken}`, // temporary
-          userInfo
-        });
-      }
     } catch(err) {
       return res.serverError(err);
     }
 
   } else {
-    if (!email) {
-      return res.badRequest({
-        message: "Missing parameters."
-      });
-    }
-
     try {
       userInfo = await Student.findOne({ email });
-      if (!userInfo) {
-        return res.conflict({
-          message: "This email is not exists."
-        });
-      } else {
-        const decodedInfo = _.assign({}, _.pick(userInfo, ['id', 'email']), { role: 'student' });
-        const verificationToken = JwtService.issue(decodedInfo, { expiresIn: '1h' });
-
-        EmailService.sendToUser(userInfo, 'verify-student-email', {
-          // verificationLink: `${process.env.WEB_URL}/email/verify?token=${verificationToken}`,
-          verificationLink: `${process.env.API_URL}/auth/verify?token=${verificationToken}`, // temporary
-          userInfo
-        });
-
-      }
     } catch(err) {
       return res.serverError(err);
     }
   }
-  res.ok(userInfo);
+
+  if (!userInfo) {
+    return res.badRequest({
+      message: "This email does not match any account."
+    });
+  } else {
+    const decodedInfo = _.assign({}, _.pick(userInfo, ['id', 'email']), { role });
+    const verificationToken = JwtService.issue(decodedInfo, { expiresIn: '1h' });
+
+    await EmailService.sendToUser(userInfo, role === 'company' ? 'verify-company-email' : 'verify-student-email', {
+      verificationLink: `${process.env.API_URL}/auth/verify?token=${verificationToken}`, // temporary
+      userInfo
+    });
+  }
+  res.ok({
+    message: "Sent email."
+  })
 
 }
