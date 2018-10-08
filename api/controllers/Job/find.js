6e8ce4c8264fea029bc104967cacc55d8e69c1de
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2018-10-08 22:15:16 +0700

    update api find job for both company and all

--- api/controllers/Job/find.js
+++ api/controllers/Job/find.js
@@ -1,11 +1,18 @@
 module.exports = async function (req, res) {
   const userId = _.get(req, "user.id");
   const role = _.get(req, "user.role");
-  const criteria = role == "company" ? { company: userId } : {};
 
+  if (userId && role == 'company') {
+    await findByCompanyId(req, res, userId);
+  } else {
+    await findAll(req, res);
+  }
+}
+
+const findByCompanyId = async (req, res, userId) => {
   try {
     const job = await Job
-      .find(criteria)
+      .find({ company: userId })
       .populate('students', { select: ['id', 'firstName', 'lastName'] })
       .populate('skills', { select: ['id', 'name'] })
       .populate('category');
@@ -15,10 +22,27 @@ module.exports = async function (req, res) {
         message: 'Job is not found'
       });
     }
+
     return res.ok(job);
   } catch (err) {
     return res.serverError({
-      message: `Something went wrong.`
+      message: `Something went wrong .`
+    });
+  }
+}
+
+const findAll = async (req, res) => {
+  try {
+    const job = await Job
+      .find({})
+      .populate('students', { select: ['id', 'firstName', 'lastName'] })
+      .populate('skills', { select: ['id', 'name'] })
+      .populate('category');
+
+    return res.ok(job);
+  } catch (err) {
+    return res.serverError({
+      message: `Something went wrong .`
     });
   }
 }
