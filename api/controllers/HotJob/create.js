cb27b84b0db2e90ead22a4d546bbf244f3cd4be8
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2018-10-30 10:52:39 +0700

    [ADMIN] create hotjob

--- api/controllers/HotJob/create.js
+++ api/controllers/HotJob/create.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 
 module.exports = async function (req, res) {
   const { expiredDay, jobId } = req.body;
-
+  let isCreated, initialExpiredDay, id;
   if (!expiredDay || !jobId) {
     return res.badRequest({
       message: "Missing parameters."
@@ -19,11 +19,56 @@ module.exports = async function (req, res) {
   }
 
   try {
+    const job = await Job.findOne({ id: jobId });
+    
+    if (!job) {
+      return res.notFound({
+        message: "Job not found."
+      });
+    }
+  } catch (err) {
+    return res.serverError({
+      message: "Something went wrong."
+    });
+  }
+
+  try {
+    const job = await HotJob.findOne({ job: jobId });
+
+    if (job) {
+      isCreated = true;
+      initialExpiredDay = _.parseInt(_.get(job, 'expiredDay'));
+      id = _.get(job, 'id');
+    } else {
+      isCreated = false;
+      initialExpiredDay = null;
+    }
+
+  } catch (err) {
+    return res.serverError({
+      message: "Something went wrong."
+    });
+  }
+
+  if (isCreated) {
+    try {
+      const hotjob = await HotJob.update({ id })
+      .set({ expiredDay: transformExpiredDay(initialExpiredDay, expiredDay) })
+      .fetch();
+
+      return res.ok(_.get(hotjob, '0'))
+    } catch (err) {
+      return res.serverError({
+        message: `Something went wrong.${err}`
+      });
+    }
+  }
+
+  try {
     const job = await HotJob.create({
       job: jobId,
-      expiredDay: transformExpiredDay(expiredDay),
+      expiredDay: transformExpiredDay(initialExpiredDay, expiredDay),
     }).fetch();
-
     return res.ok(job);
   } catch (err) {
     return res.serverError({
@@ -32,10 +77,13 @@ module.exports = async function (req, res) {
   }
 };
 
-const transformExpiredDay = type => {
+const transformExpiredDay = (initialExpiredDay, type) => {
+  if (!initialExpiredDay) {
+    initialExpiredDay = moment.now()
+  }
   switch (type) {
-    case ONE_WEEK: return moment().add(1, 'w').valueOf();
-    case TWO_WEEKS: return moment().add(2, 'w').valueOf();
-    case ONE_MONTH: return moment().add(1, 'M').valueOf();
+    case ONE_WEEK: return moment(initialExpiredDay).add(1, 'w').valueOf();
+    case TWO_WEEKS: return moment(initialExpiredDay).add(2, 'w').valueOf();
+    case ONE_MONTH: return moment(initialExpiredDay).add(1, 'M').valueOf();
   }
 }
