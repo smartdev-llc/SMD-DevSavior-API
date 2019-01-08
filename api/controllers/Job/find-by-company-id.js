4b1cefcefd4d37fc91b52412ab2b028f8165ba65
Dung Tran <dungtran.tien@pivot88.com>
2019-01-09 01:33:13 +0700

    [JOB] find job by admin/company

--- api/controllers/Job/find-by-company-id.js
+++ api/controllers/Job/find-by-company-id.js
@@ -1,7 +1,13 @@
+/**
+ * For student to find jobs created by a specific company
+ */
+
 const {
   MISSING_PARAMETERS,
   INTERNAL_SERVER_ERROR
 } = require('../../../constants/error-code');
+const constants = require('../../../constants');
+const { ACTIVE } = constants.STATUS;
 
 module.exports = async function (req, res) {
   const companyId = _.get(req, "params.companyId");
@@ -18,15 +24,28 @@ module.exports = async function (req, res) {
   }
 
   try {
-    const job = await Job
-    .find({ company: companyId })
+    const where = {
+      company: companyId, status: ACTIVE
+    }
+
+    const total = await Job.count({}).where(where);
+
+    const jobs = await Job
+    .find({})
+    .where(where)
     .populate('skills', { select: ['id', 'name'] })
     .populate('category')
     .populate('company')
+    .sort('createdAt DESC')
     .limit(limit)
     .skip(skip);
 
-    return res.ok(job);
+    return res.ok({
+      total,
+      list: jobs,
+      size,
+      page
+    });
   } catch (err) {
     return res.serverError({
       message: `Something went wrong.`,
--- api/controllers/Job/find.js
+++ api/controllers/Job/find.js
@@ -1,63 +1,55 @@
 const {
-  INTERNAL_SERVER_ERROR
+  INTERNAL_SERVER_ERROR,
+  INVALID_PARAMETERS
 } = require('../../../constants/error-code');
 
+const constants = require('../../../constants');
+const { PENDING, REJECTED, ACTIVE, INACTIVE } = constants.STATUS
+
 module.exports = async function (req, res) {
-  const userId = _.get(req, "user.id");
-  const role = _.get(req, "user.role");
-  const { size, page } = _.get(req, 'query');
+  const { size, page, status } = _.get(req, 'query');
   let limit = parseInt(size) || 10;
   let skip = (parseInt(page) || 0) * limit;
 
-  if (userId && role === 'company') {
-    await findByCompanyId(req, res, userId, limit, skip);
-  } else {
-    await findAll(req, res, limit, skip);
-  }
-}
-
-const findByCompanyId = async (req, res, userId, limit, skip) => {
-  try {
-    const jobs = await Job
-      .find({ company: userId })
-      .populate('students', { select: ['id', 'firstName', 'lastName'] })
-      .populate('skills', { select: ['id', 'name'] })
-      .populate('category')
-      .populate('company')
-      .limit(limit)
-      .skip(skip);
-
-    return res.ok(jobs);
-  } catch (err) {
-    return res.serverError({
-      message: `Something went wrong .`,
-      devMessage: err.message,
-      code: INTERNAL_SERVER_ERROR
-    });
-  }
-}
-
-const findAll = async (req, res, limit, skip) => {
-  try {
-    let jobs = await Job
-      .find({})
-      .populate('students', { select: ['id', 'firstName', 'lastName'] })
-      .populate('skills', { select: ['id', 'name'] })
-      .populate('category')
-      .populate('company')
-      .limit(limit)
-      .skip(skip);
-
-    jobs = _.map(jobs, job => {
-      job.numberOfCandidates = _.size(_.get(job, 'students'))
-      return _.omit(job, ['students']);;
+  if (status && !isValidStatus(status)) {
+    return res.badRequest({
+      message: `Invalid status.`,
+      devMessage: `Invalid status (should be PENDING | REJECTED | ACTIVE | INACTIVE)`,
+      code: INVALID_PARAMETERS
     })
-    return res.ok(jobs);
+  }
+
+  try {
+    const where = {
+      status
+    }
+
+    const total = await Job.count({}).where(where);
+
+    const jobs = await Job
+    .find({})
+    .where(where)
+    .populate('skills', { select: ['id', 'name'] })
+    .populate('category')
+    .populate('company')
+    .sort('createdAt DESC')
+    .limit(limit)
+    .skip(skip);
+
+    return res.ok({
+      total,
+      list: jobs,
+      size,
+      page,
+      status
+    });
   } catch (err) {
     return res.serverError({
-      message: `Something went wrong .`,
+      message: `Something went wrong.`,
       devMessage: err.message,
       code: INTERNAL_SERVER_ERROR
     });
   }
 }
+
+isValidStatus = (stt) => _.indexOf([PENDING, REJECTED, ACTIVE, INACTIVE], stt) > -1;
