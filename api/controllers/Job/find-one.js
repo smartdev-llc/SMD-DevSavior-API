e8ee59afa4fa9314a0d6079e3c1a45774b0278e2
Dung Tran <dungtran.tien@pivot88.com>
2019-01-10 20:39:54 +0700

    [JOB] Return students on api to get jobs + Check status to get job detail

--- api/controllers/Job/find-one.js
+++ api/controllers/Job/find-one.js
@@ -1,76 +1,75 @@
+const constants = require('../../../constants');
+const { ACTIVE } = constants.STATUS;
+
+const {
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND
+} = require('../../../constants/error-code');
+
 module.exports = async function (req, res) {
   const userId = _.get(req, "user.id");
   const role = _.get(req, "user.role");
   const id = _.get(req, "params.id");
 
-  if (userId && role == 'company') {
-    await findOneByCompanyId(req, res, userId, id);
-  } else {
-    await findOne(req, res, userId, id);
+  let condition = {
+    id
   }
-}
 
-const findOneByCompanyId = async (req, res, userId, id) => {
+  if (userId) {
+    if (role === 'company') {
+      condition.company = userId;
+    } else if (role !== 'admin') {
+      condition.status = ACTIVE;
+    } 
+  } else {
+    condition.status = ACTIVE;
+  }
+
+  let job;
+
   try {
-    const job = await Job
-      .findOne({ id, company: userId })
-      .populate('company')
-      .populate('students', { select: ['id', 'firstName', 'lastName'] })
-      .populate('skills', { select: ['id', 'name'] })
-      .populate('category');
+    if (role === 'company' || role === 'admin') {
+      job = await Job
+        .findOne(condition)
+        .populate('company')
+        .populate('students', { select: ['id', 'firstName', 'lastName'] })
+        .populate('skills', { select: ['id', 'name'] })
+        .populate('category');
+    } else {
+      job = await Job
+        .findOne(condition)
+        .populate('company')
+        .populate('skills', { select: ['id', 'name'] })
+        .populate('category');
+    }
 
     if (!job) {
       return res.notFound({
-        message: 'Job is not found.'
+        message: 'Job is not found.',
+        devMessage: 'Job is not found.',
+        code: NOT_FOUND
       });
+    }
+
+    if (role === 'student') {
+      try {
+        const application = await JobApplication.findOne({ student: userId, job: id });
+        job.isApplied = !!application;
+      } catch (err) {
+        return res.serverError({
+          message: "Something went wrong.",
+          devMessage: err.message,
+          code: INTERNAL_SERVER_ERROR
+        });
+      }
     }
 
     return res.ok(job);
   } catch (err) {
     return res.serverError({
-      message: `Something went wrong .${err}`
+      message: "Something went wrong.",
+      devMessage: err.message,
+      code: INTERNAL_SERVER_ERROR
     });
   }
-}
-
-const findOne = async (req, res, userId, id) => {
-  let job;
-  let isApplied;
-  try {
-    job = await Job
-      .findOne({ id })
-      .populate('company')
-      .populate('skills', { select: ['id', 'name'] })
-      .populate('category');
-
-    if (!job) {
-      return res.notFound({
-        message: 'Job is not found.'
-      });
-    }
-
-  } catch (err) {
-    return res.serverError({
-      message: `Something went wrong .`
-    });
-  }
-
-  try {
-    const jobApplication = await JobApplication
-      .findOne({ student: !!userId ? userId : null, job: id });
-      
-    if (!jobApplication) {
-      isApplied = false;
-    } else {
-      isApplied = true;
-    }
-
-  } catch (err) {
-    return res.serverError({
-      message: `Something went wrong .`
-    });
-  }
-  job.isApplied = isApplied;
-  return res.ok(job);
-
 }
