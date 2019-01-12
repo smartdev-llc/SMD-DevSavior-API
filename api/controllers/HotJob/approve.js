5f3de7b99d72f162b32bd081c7e1f10af03f0338
Chuong Vo <chuong.2v@gmail.com>
2019-01-12 20:05:09 +0700

    [hotjob.approve] - check if reach limited

--- api/controllers/HotJob/approve.js
+++ api/controllers/HotJob/approve.js
@@ -3,6 +3,7 @@
  - default duration is 1 day
  - default status is PENDING
  - need Back-office's operator to approve
+ - at a moment, there are 15 hot jobs only
 */
 
 const constants = require("../../../constants");
@@ -13,16 +14,16 @@ const debuglog = require("debug")("jv:hotjob:approve");
 module.exports = async function (req, res) {
   try {
     const { id } = req.params;
-    // let job = await Job.findOne({
-    //   id: jobId,
-    //   status: constants.STATUS.ACTIVE
-    // });
-    // if (!job) {
-    //   return res.notFound({
-    //     message: "Job not found.",
-    //     code: "NOT_FOUND"
-    //   });
-    // }
+    let count = await HotJob.count({
+      status: APPROVED,
+      expiredAt: { ">": moment.now() }
+    });
+    if (count >= 15) {
+      return res.notFound({
+        message: "At a moment, there are 15 hot jobs only",
+        code: "LIMITED_HOTJOB"
+      });
+    }
     let hotJob = await HotJob.updateOne({
       id: id,
       status: PENDING
@@ -32,10 +33,13 @@ module.exports = async function (req, res) {
       expiredDay: moment().add(1, "day").valueOf(),
       approvedAt: +new Date()
     });
-    
+
     debuglog("- hotJob ", hotJob);
 
-    res.ok(hotJob);
+    hotJob ? res.ok(hotJob) : res.notFound({
+      message: "Request hot job not found",
+      code: "NOT_FOUND"
+    });
   } catch (error) {
     return res.serverError({
       message: `Something went wrong.`,
