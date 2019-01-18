aeb2475ed50ed7b37d0644e25e5468597cc7347a
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2019-01-18 17:58:36 +0700

    Auto send mail to company when student applied their job

--- api/controllers/JobApplication/create.js
+++ api/controllers/JobApplication/create.js
@@ -5,9 +5,22 @@ const {
   INTERNAL_SERVER_ERROR
 } = require('../../../constants/error-code');
 
-module.exports = async function (req, res) {
-  const userId = _.get(req, 'user.id');
+const { STATUS } = require("./../../../constants");
 
+const sendEmailToCompany = (job, company, user) => {
+  const contentData = {
+    job,
+    company,
+    user,
+    jobLink: `${process.env.WEB_URL}/employer/jobs/${job.id}`,
+    applicantLink: `${process.env.WEB_URL}/employer/jobs/${job.id}/candidates/${user.id}`
+  };
+  EmailService.sendToUser( { email: company.email }, 'new-candidate-email', contentData);
+};
+
+module.exports = async function (req, res) {
+  const user = _.get(req, 'user');
+  const userId = _.get(req, 'user.id');
   const { jobId } = req.params;
 
   if (!jobId || jobId === 'undefined') {
@@ -20,19 +33,27 @@ module.exports = async function (req, res) {
 
   let job;
   try {
-    job = await Job.findOne({ id: jobId });
+    job = await Job.findOne({ id: jobId }).populate("company");
   } catch (err) {
     return res.serverError({
       message: "Something went wrong.",
       devMessage: err.message,
       code: INTERNAL_SERVER_ERROR
-    })
+    });
   }
 
   if (!job) {
     return res.badRequest({
       message: "Invalid job.",
       devMessage: "Invalid job id, can not found.",
+      code: INVALID_PARAMETERS
+    });
+  }
+
+  if (job.status !== STATUS.ACTIVE) {
+    return res.badRequest({
+      message: "Invalid job.",
+      devMessage: "Invalid job id, status is not active.",
       code: INVALID_PARAMETERS
     });
   }
@@ -45,27 +66,29 @@ module.exports = async function (req, res) {
         message: "You have already applied this job.",
         devMessage: "You have already applied this job.",
         code: ALREADY_APPLIED
-      })
+      });
     }
   } catch (err) {
     return res.serverError({
       message: "Something went wrong.",
       devMessage: err.message,
       code: INTERNAL_SERVER_ERROR
-    })
+    });
   }
 
   try {
     await Job.addToCollection(jobId, 'students').members([userId]);
 
+    sendEmailToCompany(job, _.get(job, 'company'), user);
+
     res.ok({
       message: "Applied " + job.title + "."
-    })
+    });
   } catch (err) {
     return res.serverError({
       message: "Something went wrong.",
       devMessage: err.message,
       code: INTERNAL_SERVER_ERROR
-    })
+    });
   }
-}
+};
