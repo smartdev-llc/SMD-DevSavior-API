5ebbaa66848587c045a9040006cab601157e4688
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2019-01-21 10:51:01 +0700

    add more fields

--- api/models/Skill.js
+++ api/models/Skill.js
@@ -4,9 +4,10 @@
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
+const constants = require('../../constants');
+const { ACTIVE, INACTIVE, PENDING } = constants.STATUS;
 
 module.exports = {
-
   attributes: {
     name: { type: 'string', unique: true, required: true },
     students: {
@@ -18,8 +19,11 @@ module.exports = {
       collection: 'job',
       via: 'skill',
       through: 'jobskill'
+    },
+    status: {
+      type: 'string',
+      isIn: [ACTIVE, INACTIVE, PENDING],
     }
   },
-  
 };
 
