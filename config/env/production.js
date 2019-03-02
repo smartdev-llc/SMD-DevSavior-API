c0cb1b5d81dcf52a756d81b2cbb22d6642e89e06
Dung Tran <dungtran.tien@pivot88.com>
2019-03-03 00:58:11 +0700

    allow trustProxy on CORS

--- config/env/production.js
+++ config/env/production.js
@@ -328,7 +328,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
