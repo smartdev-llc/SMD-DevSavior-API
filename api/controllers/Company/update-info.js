053a09a22bfd78ec8d5949524197b60e8f25cd41
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2019-03-28 15:44:26 +0700

    Fix error on automatically update slug on updating the same company name

--- api/controllers/Company/update-info.js
+++ api/controllers/Company/update-info.js
@@ -1,10 +1,10 @@
-const slugify = require('slugify');
-const shortid = require('shortid');
+const slugify = require("slugify");
+const shortid = require("shortid");
 const {
   MISSING_PARAMETERS,
   PERMISSION_DENIED,
   INTERNAL_SERVER_ERROR
-} = require('../../../constants/error-code');
+} = require("../../../constants/error-code");
 
 module.exports = async function (req, res) {
   const companyUser = _.get(req, "user");
@@ -21,8 +21,7 @@ module.exports = async function (req, res) {
     description,
     videoURL
   } = req.body;
-
-  if ((role === 'company') && (userId !== companyId)) {
+  if ((role === "company") && (userId !== _.parseInt(companyId))) {
     return res.unauthorized({
       message: "Permission denied.",
       devMessage: "You are not allowed to do this action.",
@@ -47,6 +46,20 @@ module.exports = async function (req, res) {
     website,
     description,
     videoURL
+  };
+
+  if (companyUser.role !== "company") {
+    try {
+      const company = await Company.findOne({ id: companyId });
+      companyUser.name = _.get(company, "name");
+      companyUser.slug = _.get(company, "slug");
+    } catch (err) {
+      return res.serverError({
+        message: "Something went wrong.",
+        devMessage: err.message,
+        code: INTERNAL_SERVER_ERROR
+      });
+    }
   }
 
   if (companyUser.name !== name || !companyUser.slug) {
@@ -59,7 +72,7 @@ module.exports = async function (req, res) {
       .set(updatedFields);
 
     ElasticsearchService.updateByQuery({
-      type: 'Job',
+      type: "Job",
       body: {
         query: {
           nested: {
@@ -73,7 +86,7 @@ module.exports = async function (req, res) {
         },
         script: {
           source:  Object.keys(updatedFields).reduce((script, key)=>{
-            return script + `ctx._source.company.${key} = '${updatedFields[key]}';`;
+            return script + `ctx._source.company.${key} = "${updatedFields[key]}";`;
           }, "")
         }
       }
@@ -87,14 +100,14 @@ module.exports = async function (req, res) {
       code: INTERNAL_SERVER_ERROR
     });
   }
-}
+};
 
 const transformCity = (reqCity) => {
   reqCity = _.toUpper(reqCity);
   switch (reqCity) {
-    case 'HN': return 'HN';
-    case 'TPHCM': return 'TPHCM';
-    case 'DN': return 'DN';
-    default: return 'OTHER';
+    case "HN": return "HN";
+    case "TPHCM": return "TPHCM";
+    case "DN": return "DN";
+    default: return "OTHER";
   }
 };
